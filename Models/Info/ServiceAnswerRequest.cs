da8ca60bd70a2052b4b7e9facf583fc0c025beec
HaoTu <hao.tu@alta.com.vn>
2025-03-22 18:12:23 +0700

    feat: add infoController and dependency classes

---
+++ Models/Info/ServiceAnswerRequest.cs
@@ -0,0 +1,16 @@
+// Decompiled with JetBrains decompiler
+// Type: tradeapi.Models.Info.ServiceAnswerRequest
+// Assembly: tradeapi, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 2B1DD9E6-779B-413A-AAC1-D3429DA62127
+// Assembly location: /Users/tunghaotu/www/service/tradeapi/tradeapi.dll
+
+#nullable enable
+namespace tradeapi.Models.Info
+{
+    public class ServiceAnswerRequest
+    {
+        public string lang { get; set; } = "EN";
+
+        public int question_pk { get; set; }
+    }
+}
